9a8b98f94aaf3b3f1c87a2ba11180fee8db0dffe
Sukrit Jaidee <45908411+SukritJaAIproject@users.noreply.github.com>
2022-03-24 12:18:27 +0700

    Update crop_face_mediapipe_vdo.py

--- crop_face/crop_face_mediapipe_vdo.py
+++ crop_face/crop_face_mediapipe_vdo.py
@@ -39,6 +39,7 @@ def calc_bounding_rect(image, landmarks):
     
 def caminfo(vdo_name):
   cap = cv2.VideoCapture(vdo_name)
+  cap. set(cv2.CV_CAP_PROP_FPS, 5)
   total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
   width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
   height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
